2a0910d2e5b4d7c4b8c800ee08e1578ccace84f6
YeetMaster467 <elicomixinc@gmail.com>
2022-10-19 19:58:01 -0700

    League Invaders in progress

---
+++ src/GamePanel.java
@@ -0,0 +1,90 @@
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener {
+
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+	int currentState = MENU;
+	Font titleFont = new Font("Arial", Font.PLAIN, 48);
+	Font normalFont = new Font("Arial", Font.PLAIN, 26);
+	Timer frameDraw;
+
+	@Override
+	public void paintComponent(Graphics g) {
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
+		}
+
+	}
+
+	GamePanel() {
+		frameDraw = new Timer(1000 / 60, this);
+		frameDraw.start();
+	}
+
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 25, 100);
+		g.setFont(normalFont);
+		g.drawString("Press ENTER to start", 100, 400);
+		g.drawString("Press SPACE for instructions", 75, 500);
+
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("Game Over", 100, 100);
+		g.setFont(normalFont);
+		g.drawString("You killed [ ] enemies", 100, 400);
+		g.drawString("Press ENTER to restart", 100, 500);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(currentState == MENU){
+		    updateMenuState();
+		}else if(currentState == GAME){
+		    updateGameState();
+		}else if(currentState == END){
+		    updateEndState();
+		}
+		System.out.println("action");
+		repaint();
+	}
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,27 @@
 
+import javax.swing.JFrame;
+
 public class LeagueInvaders {
 
+	JFrame frame;
+	GamePanel panel;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+
+	public static void main(String[] args) {
+		LeagueInvaders game = new LeagueInvaders();
+		game.setup();
+	}
+
+	LeagueInvaders() {
+		frame = new JFrame();
+	}
+
+	void setup() {
+		panel = new GamePanel();
+		frame.add(panel);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
 }
